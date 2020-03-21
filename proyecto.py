875c1868f5339acf1ea6dc97b657f77010670dbf
estebancabreraa <esteban.cabrera.a@gmail.com>
2020-03-21 15:58:07 -0600

    proyecto.py creado con librerias incluidas

---
+++ proyecto.py
@@ -0,0 +1,15 @@
+################################################################################
+#                                    PROYECTO 1                                #
+################################################################################
+#BASES DE DATOS
+
+#INTEGRANTES:
+#Esteban Cabrera - 17781
+#Andrea Elias - 17046
+#Kevin Macario - 17163
+
+import tkinter as tk
+from tkinter import messagebox
+import psycopg2
+from psycopg2 import Error
+from random import randint
