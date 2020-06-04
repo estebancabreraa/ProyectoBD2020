adacabdf2cc3efe7836d05c29cb5393d4ea45777
Mac17369 <Mac17369@uvg.edu.gt>
2020-06-03 20:52:15 -0600

    bla

--- recomendacion.py
+++ recomendacion.py
@@ -8,7 +8,7 @@ from menuAdmin import *
 from reporteria import *
 from menuUsuario import *
 
-def recomendacion(genero):
+def recomendacion(artista):
     try:
         connection = psycopg2.connect(user = "postgres",
                                       password = "123456",
@@ -16,15 +16,17 @@ def recomendacion(genero):
                                       port = "5433",
                                       database = "proyecto2")
         cursor = connection.cursor()
-        create_table_query = ''''''
+        create_table_query = '''SELECT  track1.rep, track1.name, artist1.name
+FROM album album1 join artist artist1 on album1.artistid = artist1.artistid join track track1 on track1.albumid = album1.albumid
+WHERE artist1.name =''' + "'" + artista + "'" + '''
+group by artist1.name, track1.rep, track1.name
+ORDER BY track1.rep DESC'''
 
         cursor.execute(create_table_query)
         result = cursor.fetchall()
 
         for row in result:
-            print("Anio =", row[0],)
-            print("Total Ventas =", row[1],)
-            print("Semana =" , row[2], "\n")
+            print("Cancion =", row[1],)
             
         connection.commit()                 
             
@@ -57,7 +59,7 @@ def ventanaRecomendacion():
     label5 = tk.Label(cancionFr, text="Genero:", font=("Courier", 13), bg="LightGreen", fg="black")
     label5.pack(side=tk.LEFT, ipady = 7)
 
-    combobox2 = ttk.Combobox(cancionFr, width=10, values = ["Rock", "Jazz", "Latin", "Pop", "World"], state = "readonly")
+    combobox2 = ttk.Combobox(cancionFr, width=10, values = ["Aerosmith", "Camila Cabello", "Metallica", "Queen", "Kiss"], state = "readonly")
     combobox2.pack(side = tk.LEFT)
     combobox2.current(0)
     
@@ -70,36 +72,14 @@ def ventanaRecomendacion():
         
 
     def geti():
-        genero = combobox2.current()
-        if(genero == 0):
-            genero = 1
-            print(genero)
-                
-        elif(genero == 1):
-            genero = 2
-            print(genero)
-                
-        elif(genero == 2):
-            genero = 7
-            print(genero)
-
-        elif(genero == 3):
-            genero = 9
-            print(genero)
-
-        elif(genero == 4):
-            genero = 16
-            print(genero)
-
-        else:
-            genero = 16
-            print(genero)
-        gen = str(genero)
+        artista = combobox2.get()
+        ar = str(artista)
         
-        recomendacion(gen)
+        recomendacion(ar)
 
     regArtista = tk.Button(venMain, text="Descubrir", command=geti)
     regArtista.config(font=("Courier", 20))
     regArtista.pack()
 
     venMain.mainloop()
+ventanaRecomendacion()
