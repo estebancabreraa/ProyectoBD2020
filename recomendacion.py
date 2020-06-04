d9772c9d6b341d902155f02ca203082a0827c710
Mac17369 <Mac17369@uvg.edu.gt>
2020-06-03 19:44:03 -0600

    iuy

---
+++ recomendacion.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+from tkinter import messagebox
+from random import randint
+from tkinter import Entry
+import psycopg2
+from psycopg2 import Error
+from menuAdmin import *
+from reporteria import *
+from menuUsuario import *
+
+def ventanaRecomendacion():
+    
+    venMain = tk.Tk()
+    venMain.title("Descubrimiento Semanal")
+    venMain.geometry("800x700")
+    venMain.configure(background="LightGreen")
+
+    #TITULO
+    labelMenu = tk.Label(venMain, text="Descubrimiento Semanal", bg="LightGreen", fg="black")
+    labelMenu.config(font=("Century", 44), pady=50)
+    labelMenu.pack()
+
+    cancionFr = tk.Frame(venMain, bg = "LightGreen")
+
+    label5 = tk.Label(cancionFr, text="Genero:", font=("Courier", 13), bg="LightGreen", fg="black")
+    label5.pack(side=tk.LEFT, ipady = 7)
+
+    combobox2 = ttk.Combobox(cancionFr, width=10, values = [20, 17, 15, 13, 10, 7, 5, 1], state = "readonly")
+    combobox2.pack(side = tk.LEFT)
+    combobox2.current(0)
+
+    label5 = tk.Label(cancionFr, text="Cantidad:", font=("Courier", 13), bg="LightGreen", fg="black")
+    label5.pack(side=tk.LEFT, ipady = 7)
+    
+    combobox2 = ttk.Combobox(cancionFr, width=10, values = [20, 17, 15, 13, 10, 7, 5, 1], state = "readonly")
+    combobox2.pack(side = tk.LEFT)
+    combobox2.current(0)
+    
+    cancionFr.pack(side=tk.TOP)
+
+    label5 = tk.Label(venMain, text="  ", font=("Courier", 13), bg="LightGreen", fg="black")
+    label5.pack()
+
+    regArtista = tk.Button(venMain, text="Descubrir", command=ventanaRegArtista)
+    regArtista.config(font=("Courier", 20))
+    regArtista.pack()
+    
+
+    venMain.mainloop()
+
+ventanaRecomendacion()
